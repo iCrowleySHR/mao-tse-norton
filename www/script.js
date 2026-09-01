1b32417d65d127b49626a1e352e402c31ed875f5
Gustavo Gualda <104951600+iCrowleySHR@users.noreply.github.com>
2026-08-31 21:18:16 -0300

    feat: colocando a radio aspen

--- www/script.js
+++ www/script.js
@@ -9,20 +9,23 @@ telaInicial.addEventListener("click", () => {
     // Esconde a tela inicial
     telaInicial.style.display = "none";
 
-    // Lista de músicas
+    // Lista de músicas / rádio
     const musicas = [
         "./msc/duran.mp3",
-        "./msc/china.mp3"
+        "./msc/china.mp3",
+        "https://24283.live.streamtheworld.com/ASPENAAC.aac"
     ];
 
-    // Escolhe uma música aleatoriamente
-    const musicaAleatoria = musicas[Math.floor(Math.random() * musicas.length)];
+    // Escolhe aleatoriamente
+    const musicaAleatoria =
+        musicas[Math.floor(Math.random() * musicas.length)];
 
     // Define a música escolhida
     musica.src = musicaAleatoria;
 
-    // Começa a música
-    musica.play();
+    // Começa a reprodução
+    musica.play().catch((erro) => {
+        console.error("Erro ao reproduzir:", erro);
+    });
 
 });
-
